c74b4c06a8bc633ff3e17887ed2a905c792d86df
Dinesh <dinesh2k95@gmail.com>
2018-11-19 06:29:04 +0530

    Dinesh | Added new data structure for mapping firebase list objects to recycler view

--- app/src/main/java/projects/dinesh/housetaskmanager/TaskFragment.kt
+++ app/src/main/java/projects/dinesh/housetaskmanager/TaskFragment.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.ViewModelProviders
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -28,6 +29,14 @@ class TaskFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        taskList.adapter = viewModel.init()
+        taskList.setHasFixedSize(false)
+        viewModel.init()
+        taskList.layoutManager = LinearLayoutManager(context)
+//        (taskList.adapter as TaskViewModel.TaskAdapter).startListening()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+//        (taskList.adapter as TaskViewModel.TaskAdapter).stopListening()
     }
 }
--- app/src/main/java/projects/dinesh/housetaskmanager/TaskViewModel.kt
+++ app/src/main/java/projects/dinesh/housetaskmanager/TaskViewModel.kt
@@ -2,55 +2,82 @@ package projects.dinesh.housetaskmanager
 
 import android.arch.lifecycle.ViewModel
 import android.support.v7.widget.RecyclerView
-import android.view.ViewGroup
-import com.firebase.ui.database.FirebaseRecyclerAdapter
-import com.firebase.ui.database.FirebaseRecyclerOptions
 import android.view.LayoutInflater
 import android.view.View
-import android.widget.TextView
+import android.view.ViewGroup
+import android.widget.CheckedTextView
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.task_line_item.view.*
 
 class TaskViewModel : ViewModel() {
 
-    private var dataBaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("tasks")
+    private var usersDbReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("users")
+    private var taskDbReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("tasks")
     lateinit var taskAdapter: TaskAdapter
 
+    fun init() {
+        taskAdapter = TaskAdapter(arrayListOf())
+        usersDbReference.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
 
-    fun init(): TaskAdapter {
-        val options = FirebaseRecyclerOptions.Builder<String>()
-            .setQuery(dataBaseReference, String::class.java)
-            .build()
-        dataBaseReference.addValueEventListener(
-            object: ValueEventListener{
-                override fun onCancelled(p0: DatabaseError) {
-
-                }
-
-                override fun onDataChange(p0: DataSnapshot) {
-                    println("Database value :::: $p0")
-                }
             }
-        )
-        taskAdapter = TaskAdapter(options)
-        return taskAdapter
-    }
-}
 
-class TaskAdapter(options: FirebaseRecyclerOptions<String>): FirebaseRecyclerAdapter<String, TaskViewHolder>(options) {
+            override fun onDataChange(p0: DataSnapshot) {
+                val typeIndicator = object : GenericTypeIndicator<ArrayList<User>>() {}
+                val arrayList: ArrayList<User> = p0.getValue(typeIndicator)!!
+                val user = arrayList.filter { it.name == "Dinesh" }[0]
+                taskAdapter.userTasks = user.taskIds
+                taskAdapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    inner class TaskAdapter(var userTasks: ArrayList<UserTask>) :
+        RecyclerView.Adapter<TaskViewHolder>()
+
+    {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.task_line_item, parent, false)
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.task_line_item, parent, false)
             return TaskViewHolder(view)
         }
 
-        override fun onBindViewHolder(holder: TaskViewHolder, position: Int, model: String) {
-            holder.taskName.text = model
+        override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+            val userTask = userTasks[position]
+            taskDbReference.child(userTask.taskId.toString())
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        val task = dataSnapshot.getValue(Task::class.java)!!
+                        val taskName = holder.taskName
+                        if (task.completed) {
+                            taskName.isChecked = true
+                        }
+                        taskName.text = task.name
+                        holder.taskName.setOnClickListener {
+                            taskName.toggle()
+                            userTask.completed = taskName.isChecked
+                            taskDbReference.child("${holder.adapterPosition}").setValue(userTask)
+                                .addOnSuccessListener { println("Toggle Successful !") }
+                                .addOnFailureListener { exception -> println("Toggle UnSuccessful ::: $exception") }
+                        }
+                    }
+
+                    override fun onCancelled(p0: DatabaseError) {
+                    }
+                })
         }
+
+        override fun getItemCount(): Int {
+            return userTasks.size
+        }
+    }
+
 }
 
-class TaskViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    val taskName: TextView = itemView.task_name
+class TaskViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    val taskName: CheckedTextView = view.checked_task_item
 }
 
-data class Task(val id: Int, val name: String, val completed: Boolean)
+data class  User(var name: String = "", var taskIds: ArrayList<UserTask> = arrayListOf())
+data class UserTask(var taskId: Int = 0, var completed: Boolean = false)
+data class Task(var name: String = "", var completed: Boolean = false)
